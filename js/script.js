12834f9fce7d644c1462afc359f56d19f6d833c2
Fabrizio <fabriziospoto1@gmail.com>
2020-07-16 21:09:30 +0200

    Definitivo
    Definitivo

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
-document.getElementById('genera').addEventListener("click", function() {
+document.getElementById('genera').addEventListener("click", function() {   //Funzioni Show
     var passengers = document.getElementById('passengers');
     passengers.classList.remove("hidden");
     passengers.classList.add("show");
 }
 );
 
-document.getElementById('annulla').addEventListener("click", function() {
+document.getElementById('annulla').addEventListener("click", function() { //FUnzione Hidden
     var passengers = document.getElementById('passengers');
     passengers.classList.remove("show");
     passengers.classList.add("hidden");
 }
 );
 
-document.getElementById('genera').addEventListener("click", function() {
+document.getElementById('genera').addEventListener("click", function() {  //Riporto Nome Utente
     var valore = document.getElementById('nomeutente').value;
     document.getElementById('nome-passeggero').innerHTML = valore;
 }
 );
 
-document.getElementById('genera').addEventListener("click", function() {
+document.getElementById('genera').addEventListener("click", function() {  //Creo funzione con If per prezzo
     var distanza = document.getElementById('km').value
     var eta = document.getElementById('old').value
     var prezzo = 0.21 * distanza;
@@ -38,13 +38,13 @@ document.getElementById('genera').addEventListener("click", function() {
 }
 );
 
-document.getElementById('genera').addEventListener("click", function(){
+document.getElementById('genera').addEventListener("click", function(){  //Genero numero random per carrozza
     var numCarrozza = Math.floor(Math.random() * 10 + 1);
     document.getElementById('carrozza').innerHTML = numCarrozza;
 }
 );
 
-document.getElementById('genera').addEventListener("click", function(){
+document.getElementById('genera').addEventListener("click", function(){ //Genero numero random per Codice CP
     var codice = Math.floor(Math.random() * (99999 - 90000 + 1)) + 90000;
     document.getElementById('ccp').innerHTML = codice;
 }
